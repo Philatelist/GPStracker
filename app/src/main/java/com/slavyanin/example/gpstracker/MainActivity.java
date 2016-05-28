d0c46eb6f56c54bfec9013d079f78cae5166c141
Alexander Usov <a.usoff@gmail.com>
2016-05-28 17:10:40 +0300

    Add Navigation Drawer to Toolbar

--- app/src/main/java/com/slavyanin/example/gpstracker/MainActivity.java
+++ app/src/main/java/com/slavyanin/example/gpstracker/MainActivity.java
@@ -2,15 +2,27 @@ package com.slavyanin.example.gpstracker;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IProfile;
+
 public class MainActivity extends AppCompatActivity{
 
-    Button btnToSmsList;
+    private Drawer drawerResult;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -18,17 +30,6 @@ public class MainActivity extends AppCompatActivity{
         setContentView(R.layout.activity_main);
 
         addToolbar();
-
-        btnToSmsList = (Button) findViewById(R.id.idBtnToSmsList);
-
-        btnToSmsList.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SmsListActivity.class);
-                startActivity(intent);
-            }
-        });
-
 
     }
 
@@ -38,5 +39,72 @@ public class MainActivity extends AppCompatActivity{
             setSupportActionBar(toolbar);
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
+
+        initializeNavigationDrawer(toolbar);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerResult != null && drawerResult.isDrawerOpen()) {
+            drawerResult.closeDrawer();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    private void initializeNavigationDrawer(Toolbar toolbar) {
+        AccountHeader headerResult = createAccountHeader();
+
+        drawerResult = new DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .withSliderBackgroundColor(getResources().getColor(R.color.md_blue_grey_100))
+                .withAccountHeader(headerResult)
+                .addDrawerItems(
+                        initializeDrawerItems()
+                )
+                .build();
+    }
+
+    @NonNull
+    private IDrawerItem[] initializeDrawerItems() {
+        return new IDrawerItem[]{new PrimaryDrawerItem()
+                .withName(R.string.nav_menu_item_home)
+                .withIdentifier(1)
+                .withIcon(R.drawable.ic_home_black_18dp),
+                new DividerDrawerItem(),
+                new SecondaryDrawerItem()
+                        .withName(R.string.nav_menu_item_history)
+                        .withIcon(R.drawable.ic_change_history_black_18dp)
+                        .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        Intent intent = new Intent(MainActivity.this, SmsListActivity.class);
+                        startActivity(intent);
+                        return false;
+                    }
+                })
+                ,
+                new SecondaryDrawerItem()
+                        .withName(R.string.nav_menu_item_settings)
+                        .withIcon(R.drawable.ic_settings_black_18dp),
+                new SecondaryDrawerItem()
+                        .withName(R.string.nav_menu_item_gprsSettings)
+                        .withIcon(R.drawable.ic_settings_remote_black_18dp)
+                };
+    }
+
+    private AccountHeader createAccountHeader() {
+        IProfile profile = new ProfileDrawerItem()
+                .withName("GPS Tracker - TK102")
+                .withIcon(R.drawable.ic_verified_user_black_18dp);
+
+        return new AccountHeaderBuilder()
+                .withActivity(this)
+                .withHeaderBackground(R.drawable.wall)
+                .addProfiles(profile)
+                .build();
     }
 }
